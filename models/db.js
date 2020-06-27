493c173602ced4d1dfe7c211556495e93ef38ca3
Shivanshhh <65827695+Shivanshhh@users.noreply.github.com>
2020-06-27 18:04:37 +0530

    form working
    frontend add-event.css problematic so commented that out for now
    
    form-> database working

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URL, (err) => {
-    if (err) {
-        console.log(`Error: ${err}`);
-    } else {
-        console.log('Mongo Connection Success');
-    }
-});
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// mongoose.connect(process.env.MONGO_URL, (err) => {
+//     if (err) {
+//         console.log(`Error: ${err}`);
+//     } else {
+//         console.log('Mongo Connection Success');
+//     }
+// });
--- routes/eventroute.js
+++ routes/eventroute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/eventschema');
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://strtiwari28:qwerty1@cluster0-8ac9j.mongodb.net/events?retryWrites=true&w=majority', { 
   useNewUrlParser: true, 
@@ -13,9 +13,12 @@ router.get("/", (req, res) => {
   res.render("add-event.ejs");
 });
 
-router.post('/', (req,res) =>{
-  const newDoc= Event.create(req.body);
+router.post('/', async (req,res) =>{
+  const newDoc= await Event.create(req.body);
   console.log("db working")
+  res.json({
+    data:newDoc
+  });
 });
 
 
