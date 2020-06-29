5a586d7168acc4369ac8f9820e4247c939c9bd07
Arushi Jain <arushijain1479@gmail.com>
2020-06-29 11:41:20 +0530

    inital function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const dotenv = require('dotenv');
-//const mainRouter = require('./routes/index');
+const mainRouter = require('./routes/index');
 const eventrouter = require('./routes/addevent')
 const currenteventrouter= require('./routes/event')
 const app = express();
@@ -15,12 +15,12 @@ require('./models/db');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/Assets'));
+app.use('Assets', express.static(__dirname + '/Assets'));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on ${process.env.PORT || '3000'}`);
 });
 
-//app.use('/', mainRouter);
+app.use('/', mainRouter);
 app.use('/addevent', eventrouter);
 app.use('/event', currenteventrouter);
