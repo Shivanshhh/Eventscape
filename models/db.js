cc9997ca59c8e00058d7ac28c78c0d663acf437c
Shivanshhh <65827695+Shivanshhh@users.noreply.github.com>
2020-07-29 03:15:21 +0530

    Final

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-mongoose.connect('mongodb+srv://strtiwari28:qwerty1@cluster0-8ac9j.mongodb.net/bahuthogaya?retryWrites=true&w=majority', (err) => {
+mongoose.connect(process.env.MONGO_URL, (err) => {
     if (err) {
         console.log(`Error: ${err}`);
     } else {
